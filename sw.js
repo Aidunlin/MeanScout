07810458aa886ad2c006b9aef04c5a10c2c4726e
Aidunlin <32206906+Aidunlin@users.noreply.github.com>
2020-01-11 15:10:28 -0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
-workbox.routing.registerRoute(new RegExp("\w*\.*$"), new workbox.strategies.NetworkFirst({cacheName: "meanscout"}));
+workbox.routing.registerRoute(new RegExp("\w*\.*$"), new workbox.strategies.StaleWhileRevalidate({cacheName: "meanscout"}));
