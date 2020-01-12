fb1ae1452efe2b81c75d52ec4f6eab9c7b442702
abai <mail.contraband@gmail.com>
2020-01-11 20:45:04 -0800

    Purge double quotes out of existence and add prettier for development

--- sw.js
+++ sw.js
@@ -1,2 +1,5 @@
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
-workbox.routing.registerRoute(new RegExp("\w*\.*$"), new workbox.strategies.StaleWhileRevalidate({cacheName: "meanscout"}));
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
+workbox.routing.registerRoute(
+  new RegExp('w*.*$'),
+  new workbox.strategies.StaleWhileRevalidate({ cacheName: 'meanscout' })
+);
