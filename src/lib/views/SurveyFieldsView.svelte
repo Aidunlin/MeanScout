b7fe1e3ed575f44b2a1c0c5997686c8d0915f1d9
Aidunlin <aidunlin@gmail.com>
2024-01-12 17:49:26 -0800

    Add preview mode to fields editor

--- src/lib/views/SurveyFieldsView.svelte
+++ src/lib/views/SurveyFieldsView.svelte
@@ -1,14 +1,16 @@
 <script lang="ts">
-  import type { IDBRecord, Survey } from "$lib";
+  import { getDefaultFieldValue, type IDBRecord, type Survey } from "$lib";
   import Button from "$lib/components/Button.svelte";
   import Container from "$lib/components/Container.svelte";
   import FieldEditor from "$lib/components/FieldEditor.svelte";
+  import FieldValueEditor from "$lib/components/FieldValueEditor.svelte";
   import Icon from "$lib/components/Icon.svelte";
 
   export let idb: IDBDatabase;
   export let surveyRecord: IDBRecord<Survey>;
 
   let disabled = false;
+  let preview = false;
 
   const countTransaction = idb.transaction(["drafts", "entries"]);
 
@@ -25,21 +27,50 @@
   function newField() {
     surveyRecord.fields = [...surveyRecord.fields, { name: "", type: "toggle" }];
   }
+
+  function togglePreview() {
+    preview = !preview;
+  }
 </script>
 
 <Container column padding>
   <h2>Fields</h2>
-  {#if disabled}
-    <span>Cannot modify fields with entries present!</span>
+  {#if preview}
+    <Container alignEnd>
+      {#each surveyRecord.fields as field (field)}
+        {#if field.type == "group"}
+          <h2>{field.name}</h2>
+          <Container alignEnd maxWidth>
+            {#each field.fields as innerField (innerField)}
+              <FieldValueEditor field={innerField} value={getDefaultFieldValue(innerField)} />
+            {/each}
+          </Container>
+        {:else}
+          <FieldValueEditor {field} value={getDefaultFieldValue(field)} />
+        {/if}
+      {/each}
+    </Container>
+  {:else}
+    {#if disabled}
+      <span>Cannot modify fields with entries present!</span>
+    {/if}
+    {#each surveyRecord.fields as field, fieldIndex (field)}
+      <FieldEditor bind:fields={surveyRecord.fields} bind:field {fieldIndex} {disabled} />
+    {/each}
   {/if}
-  {#each surveyRecord.fields as field, fieldIndex (field)}
-    <FieldEditor bind:fields={surveyRecord.fields} bind:field {fieldIndex} {disabled} />
-  {/each}
 </Container>
 
 <footer>
   <Button title="New field" {disabled} on:click={newField}>
     <Icon name="plus" />
     Field
+  </Button>
+  <Button title="Preview" on:click={togglePreview}>
+    {#if preview}
+      <Icon name="square-check" />
+    {:else}
+      <Icon style="regular" name="square" />
+    {/if}
+    Preview
   </Button>
 </footer>
